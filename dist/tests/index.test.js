e1ba8db547fcfc6299055479d4f26ef38842da7d
roll1226 <tomo_yasu_4724@icloud.com>
2024-06-19 06:53:30 +0900

    feat: test flow

--- dist/tests/index.test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("../src/index");
-test("adds 1 + 2 to equal 3", () => {
-    expect((0, index_1.add)(1, 2)).toBe(3);
-});
