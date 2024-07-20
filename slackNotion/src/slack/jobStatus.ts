36efaf4f7191ac3fe7e910c3ae73e831feb860b3
roll1226 <tomo_yasu_4724@icloud.com>
2024-07-20 12:55:09 +0900

    check: output

--- slackNotion/src/slack/jobStatus.ts
+++ slackNotion/src/slack/jobStatus.ts
@@ -6,22 +6,17 @@ export const JOB_STATUS = {
 } as const;
 export type JOB_STATUS = (typeof JOB_STATUS)[keyof typeof JOB_STATUS];
 
-export const jobStatus: Record<JOB_STATUS, { color: string; result: string }> =
-  {
-    started: {
-      color: "#3ea8ff",
-      result: "Start",
-    },
-    succeeded: {
-      color: "#2cbe4e",
-      result: "Succeeded",
-    },
-    failed: {
-      color: "#cb2431",
-      result: "Failed",
-    },
-    cancelled: {
-      color: "#ffc107",
-      result: "Cancelled",
-    },
-  };
+export const jobStatus: Record<JOB_STATUS, { color: string }> = {
+  [JOB_STATUS.START]: {
+    color: "#3ea8ff",
+  },
+  [JOB_STATUS.SUCCESS]: {
+    color: "#2cbe4e",
+  },
+  [JOB_STATUS.FAILURE]: {
+    color: "#cb2431",
+  },
+  [JOB_STATUS.CANCELLED]: {
+    color: "#ffc107",
+  },
+};
