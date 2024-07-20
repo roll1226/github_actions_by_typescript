90e47fab44eff69ebd10dc2befe36d78a5b7f4db
roll1226 <tomo_yasu_4724@icloud.com>
2024-07-20 12:27:52 +0900

    change: message

--- slackNotion/src/slack.ts
+++ slackNotion/src/slack.ts
@@ -1,40 +1,104 @@
 import * as core from "@actions/core";
 import axios from "axios";
+import { baseAttachment } from "./slack/baseAttachment";
+import { JOB_STATUS } from "./slack/jobStatus";
+import {
+  SendOrUpdateSlackNotification,
+  SlackApi,
+  SlackAttachment,
+  SlackPayload,
+} from "./types/slack";
+
+const SLACK_API = {
+  POST: "https://slack.com/api/chat.postMessage",
+  UPDATE: "https://slack.com/api/chat.update",
+};
+
+const slackApi: SlackApi = async ({ url, payload, token }) => {
+  return await axios.post(url, payload, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json; charset=utf-8",
+    },
+  });
+};
+
+const sendOrUpdateSlackNotification: SendOrUpdateSlackNotification = async (
+  token,
+  channel,
+  attachments,
+  threadTs
+) => {
+  try {
+    const slackPayload: SlackPayload = {
+      channel,
+      attachments,
+      token,
+    };
+
+    if (threadTs) slackPayload.ts = threadTs;
+
+    const response = await slackApi({
+      url: threadTs ? SLACK_API.UPDATE : SLACK_API.POST,
+      payload: slackPayload,
+      token,
+    });
+
+    if (response.data.ok) {
+      core.setOutput("slack-thread-ts", response.data.ts);
+      core.info("Message sent successfully");
+      return response.data.ts;
+    } else {
+      core.setFailed(`Slack API error: ${response.data.error}`);
+    }
+  } catch (error) {
+    core.setFailed(
+      `Failed to send Slack notification: ${(error as Error).message}`
+    );
+  }
+};
 
 async function run() {
   try {
     const slackToken = core.getInput("slack-token");
     const slackChannel = core.getInput("slack-channel");
-    const status = core.getInput("status");
+    const status = core.getInput("status") as JOB_STATUS;
     const runId = core.getInput("run-id");
     const jobName = core.getInput("job-name");
     const repository = core.getInput("repository");
     const ref = core.getInput("ref");
     const eventName = core.getInput("event-name");
     const workflow = core.getInput("workflow");
+    const targetThreadTs = core.getInput("slack-thread-ts");
 
-    const message = `Job *${jobName}* with run ID *${runId}* has ${status}. Repository: ${repository}, Ref: ${ref}, Event: ${eventName}, Workflow: ${workflow}`;
-
-    const response = await axios.post(
-      "https://slack.com/api/chat.postMessage",
-      {
-        channel: slackChannel,
-        text: message,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${slackToken}`,
-          "Content-Type": "application/json",
-        },
-      }
+    const attachment = baseAttachment(
+      status,
+      repository,
+      ref,
+      eventName,
+      workflow,
+      runId
     );
 
-    if (response.data.ok) {
-      core.setOutput("slack-thread-ts", response.data.ts);
-      core.info("Message sent successfully");
-    } else {
-      core.setFailed(`Slack API error: ${response.data.error}`);
-    }
+    const attachments: SlackAttachment[] = [
+      {
+        pretext: ``,
+        color: attachment.color,
+        fields: attachment.fields,
+      },
+    ];
+
+    attachments[0].pretext = `Job ${jobName} with run ID ${runId} has ${status}.`;
+
+    const threadTs = await sendOrUpdateSlackNotification(
+      slackToken,
+      slackChannel,
+      attachments,
+      targetThreadTs
+    );
+
+    if (threadTs && status === JOB_STATUS.START)
+      core.setOutput("slack-thread-ts", threadTs);
   } catch (error) {
     core.setFailed(`Action failed with error: ${(error as Error).message}`);
   }
