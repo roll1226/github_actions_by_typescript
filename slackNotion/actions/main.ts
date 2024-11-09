06f4de19d0f017cf961aad0ad4c00dc4ab78bb05
roll1226 <tomo_yasu_4724@icloud.com>
2024-11-09 20:21:44 +0900

    test: action

--- slackNotion/actions/main.ts
+++ slackNotion/actions/main.ts
@@ -11,7 +11,6 @@ async function run() {
     console.log(`Payload: ${JSON.stringify(payload, null, 2)}`);
 
     // インプットパラメータを取得
-
     const whoToGreet = core.getInput("who-to-greet");
     console.log(`Hello ${whoToGreet}!`);
 
